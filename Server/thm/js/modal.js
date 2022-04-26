40f45208f324adff8b2fdfd57b5048fcbf75d0ee
las-nish <iamlasan_private@protonmail.com>
2022-04-26 13:37:48 +0530

    CSS file for modal has been created

---
+++ Server/thm/js/modal.js
@@ -0,0 +1,69 @@
+// Edit Dashboard Wiki Modal
+
+var modal_edit_dashboard_wiki = document.getElementById("edit-dashboard-wiki");
+var btn_edit_dashboard_wiki = document.getElementById("btn-edit-dashboard-wiki");
+var span_edit_dashboard_wiki = document.getElementById("edit-dashboard-wiki-close");
+
+btn_edit_dashboard_wiki.onclick = function () {
+  modal_edit_dashboard_wiki.style.display = "block";
+}
+
+span_edit_dashboard_wiki.onclick = function () {
+  modal_edit_dashboard_wiki.style.display = "none";
+}
+
+// Create New Project Modal
+
+var modal_create_new_project = document.getElementById("create-new-project");
+var btn_create_new_project = document.getElementById("btn-create-new-project");
+var span_create_new_project = document.getElementById("create-new-project-close");
+
+btn_create_new_project.onclick = function () {
+  modal_create_new_project.style.display = "block";
+}
+
+span_create_new_project.onclick = function () {
+  modal_create_new_project.style.display = "none";
+}
+
+// User Profile Modal
+
+var modal_user_profile = document.getElementById("user-profile");
+var btn_user_profile = document.getElementById("btn-user-profile");
+var span_user_profile = document.getElementById("user-profile-close");
+
+btn_user_profile.onclick = function () {
+  modal_user_profile.style.display = "block";
+}
+
+span_user_profile.onclick = function () {
+  modal_user_profile.style.display = "none";
+}
+
+// Server Profile Modal
+
+var modal_server_profile = document.getElementById("server-profile");
+var btn_server_profile = document.getElementById("btn-server-profile");
+var span_server_profile = document.getElementById("server-profile-close");
+
+btn_server_profile.onclick = function () {
+  modal_server_profile.style.display = "block";
+}
+
+span_server_profile.onclick = function () {
+  modal_server_profile.style.display = "none";
+}
+
+// Server Team Modal
+
+var modal_server_team = document.getElementById("server-team");
+var btn_server_team = document.getElementById("btn-server-team");
+var span_server_team = document.getElementById("server-team-close");
+
+btn_server_team.onclick = function () {
+  modal_server_team.style.display = "block";
+}
+
+span_server_team.onclick = function () {
+  modal_server_team.style.display = "none";
+}
